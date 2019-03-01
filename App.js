1fe0911a044d8e4b8d0b8dd514a715b2466c582d
jackding96 <jackding96@outlook.com>
2019-02-28 23:48:24 -0500

    get podcast feed api from itunes

--- App.js
+++ App.js
@@ -2,6 +2,22 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    fetch('https://itunes.apple.com/search?term=new+york&media=podcast')
+      .then(r => r.json())
+      .then((rJson) => {
+        console.log(rJson.results.map((el) => {
+          return {
+            name: el.artistName,
+            feed: el.feedUrl,
+            artwork: el.artworkUrl600,
+          };
+        }));
+      });
+  }
   render() {
     return (
       <View style={styles.container}>
