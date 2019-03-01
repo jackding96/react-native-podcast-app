34ebf1ef8e2097546e2f374e2bc9d92d6677d274
jackding96 <jackding96@outlook.com>
2019-02-28 23:57:06 -0500

    input field

--- App.js
+++ App.js
@@ -1,27 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      text: ''
+    }
   }
   componentDidMount() {
     fetch('https://itunes.apple.com/search?term=new+york&media=podcast')
       .then(r => r.json())
       .then((rJson) => {
-        console.log(rJson.results.map((el) => {
+        rJson.results.map((el) => {
           return {
             name: el.artistName,
             feed: el.feedUrl,
             artwork: el.artworkUrl600,
           };
-        }));
+        });
       });
   }
   render() {
     return (
       <View style={styles.container}>
-        <Text>HI :D</Text>
+        <TextInput
+          style={styles.inputBox}
+          placeholder={'Search a Podcast!'}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
       </View>
     );
   }
@@ -29,9 +37,11 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
   },
+  inputBox: {
+    width: "100%",
+    height: 40,
+    paddingLeft: 20
+  }
 });
